be0d73827d5307b6e80fa561e6c1b0fd472f9154
Salvador Macias (Softtek) <salvador.macias@ihg.com>
2016-06-27 17:03:45 -0500

    loading modal and message modal

--- webapp/js/controller/indexController.js
+++ webapp/js/controller/indexController.js
@@ -1,7 +1,5 @@
-myApp.controller('indexController', ['$scope','photographers', 'message', function($scope, photographers, message){
+myApp.controller('indexController', ['$scope', '$timeout','photographers', 'message', function($scope, $timeout, photographers, message){
   $scope.message = message;
-  $scope.title = 'Picture Viewer';
-  $scope.description = 'Check out our pictures taken around the world';
   $scope.redirectHome = function(){
     window.location = '/Home';
   };
@@ -10,13 +8,74 @@ myApp.controller('indexController', ['$scope','photographers', 'message', functi
     $scope.photographers = data;
   });
 
+  $scope.modalHeader = "";
+  $scope.modalMessage = "";
+  $scope.modalIcon = "";
+  $scope.modalIconColor = "";
+
+  $scope.showSuccessMessage = function(msj){
+    closeLoadingModal();
+    $timeout(function() {
+      $scope.modalHeader = message.getMsg('app_modal_header_success');
+      $scope.modalMessage = msj;
+      $scope.modalIcon = 'fa-check-square-o';
+      $scope.modalIconColor = '#4CAF50';
+      jQuery('#myModal').modal('show');
+    }, 500);
+  };
+
+  $scope.showErrorMessage = function(msj){
+    closeLoadingModal();
+    $timeout(function() {
+      $scope.modalHeader = message.getMsg('app_modal_header_error');
+      $scope.modalMessage = msj;
+      $scope.modalIcon = 'fa-exclamation-triangle';
+      $scope.modalIconColor = '#ED1B2E';
+      $scope.$apply();
+      jQuery('#myModal').modal('show');
+    }, 500);
+  };
+
+  $scope.showInfoMessage = function(msj){
+    closeLoadingModal();
+    $timeout(function() {
+      $scope.modalHeader = message.getMsg('app_modal_header_info');
+      $scope.modalMessage = msj;
+      $scope.modalIcon = 'fa-info-circle';
+      $scope.modalIconColor = '#4169E1';
+      $scope.$apply();
+      jQuery('#myModal').modal('show');
+    }, 500);
+  };
+
 }]);
+
+function showMessage(message, type){
+  if(type == 'I'){
+    angular.element($('body')).scope().showInfoMessage(message);
+  }else if (type == 'S') {
+    angular.element($('body')).scope().showSuccessMessage(message);
+  }else if (type == 'E') {
+    angular.element($('body')).scope().showErrorMessage(message);
+  }
+}
+
+function showLoadingModal(){
+  $('#loadingModal').modal({
+    backdrop: 'static',
+    keyboard: false
+  })
+}
+
+function closeLoadingModal(){
+  $('#loadingModal').modal('hide');
+}
 
 myApp.controller('albumsController', ['$scope', '$http', function($scope, $http){
    $http.get('/getAlbums').success(function(data){
     $scope.albums = data;
   }).error(function(err){
-    console.log(err);
+    showMessage('We Couldn´t Load Albums, Try Later, ', 'E');
   });
 }]);
 
@@ -45,6 +104,7 @@ myApp.controller('contactController', ['$scope', '$http', function($scope, $http
         && $scope.validateInput($scope.email)
           && $scope.validateInput($scope.msgText)){
 
+      showLoadingModal();
       $http({
             url: "https://formspree.io/salvador.macias.91@gmail.com",
             data: {
@@ -60,9 +120,25 @@ myApp.controller('contactController', ['$scope', '$http', function($scope, $http
                 "Content-Type": "application/json"
             }
         }).success(function(response){
-            console.log(response);
+            showMessage('Mail Sent Successfully!', 'S');
+            $scope.name = {
+              validClass: hide,
+              classError: '',
+              value: ''
+            };
+            $scope.email = {
+              validClass: hide,
+              validEmailClass: hide,
+              classError: '',
+              value: ''
+            };
+            $scope.msgText = {
+              validClass: hide,
+              classError: '',
+              value: ''
+            };
         }).error(function(error){
-            console.log(error);
+            showMessage('Something Went Wrong, Try Again Later', 'E');
         });
     }
     return false;
--- webapp/js/services/photographers.js
+++ webapp/js/services/photographers.js
@@ -2,6 +2,6 @@ myApp.factory('photographers', ['$http', function($http){
   return $http.get('/getPhotographers').success(function(data){
     return data;
   }).error(function(err){
-    return err;
+    showMessage('Could Not Load Photographers Information, Try Later Please', 'E');
   });
 }]);
