d00371f8893d89286056f0a7d230a4c789bdc908
Salvador Macias (Softtek) <salvador.macias@ihg.com>
2016-06-30 16:13:49 -0500

    lowdb dependency and multiple fixes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var lowdb = require('lowdb');
 var path = require('path');
 var app = express();
 var nunjucks = require('nunjucks');
@@ -8,45 +9,69 @@ var msjs = require('./resources/language/messages_EN.json');
 var homeController = require('./controllers/homeController');
 var imageController = require('./controllers/imageController');
 
-app.use('/img',  express.static(__dirname + '/webapp/img'));
-app.use('/js',  express.static(__dirname + '/webapp/js'));
-app.use('/views',  express.static(__dirname + '/webapp/views'));
-app.use('/css',  express.static(__dirname + '/webapp/css'));
+declareMiddlewares();
 
-app.use('/vendor',  express.static(__dirname + '/bower_components'));
+configNunjucks();
 
-app.set('webapp', __dirname + '/webapp');
+interceptor();
 
-var nun = nunjucks.configure('webapp', {
-    autoescape: true,
-    express: app,
-    tags: {
-        blockStart: '<%',
-        blockEnd: '%>',
-        variableStart: '<$',
-        variableEnd: '$>',
-        commentStart: '<#',
-        commentEnd: '#>'
-    }
-});
+redirectHome();
 
-//interceptor*******************
-app.use(function(req, res, next) {
-  res.locals.app_title = msjs.app_title;
-  res.locals.app_lang = msjs.app_lang;
-  res.locals.messages = JSON.stringify(msjs);
-  next();
-});
-//******************************
+controllersListen();
 
-app.get('/', function(req, res){
-  res.status(301);
-  res.redirect('/Home');
-});
+startServer();
 
-homeController.listen(app);
-imageController.listen(app);
+function declareMiddlewares(){
+  app.use('/img',  express.static(__dirname + '/webapp/img'));
+  app.use('/js',  express.static(__dirname + '/webapp/js'));
+  app.use('/views',  express.static(__dirname + '/webapp/views'));
+  app.use('/css',  express.static(__dirname + '/webapp/css'));
 
-app.listen(3000, function(){
-  console.log('AngularApp started on port 3000');
-});
+  app.use('/vendor',  express.static(__dirname + '/bower_components'));
+
+  app.set('webapp', __dirname + '/webapp');
+}
+
+function configNunjucks(){
+  var nun = nunjucks.configure('webapp', {
+      autoescape: true,
+      express: app,
+      tags: {
+          blockStart: '<%',
+          blockEnd: '%>',
+          variableStart: '<$',
+          variableEnd: '$>',
+          commentStart: '<#',
+          commentEnd: '#>'
+      }
+  });
+}
+
+function interceptor() {
+  //interceptor*******************
+  app.use(function(req, res, next) {
+    res.locals.app_title = msjs.app_title;
+    res.locals.app_lang = msjs.app_lang;
+    res.locals.messages = JSON.stringify(msjs);
+    next();
+  });
+  //******************************
+}
+
+function redirectHome() {
+  app.get('/', function(req, res){
+    res.status(301);
+    res.redirect('/Home');
+  });
+}
+
+function controllersListen(){
+  homeController.listen(app);
+  imageController.listen(app);
+}
+
+function startServer(){
+  app.listen(3000, function(){
+    console.log('AngularApp started on port 3000');
+  });
+}
