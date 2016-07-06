28c415f283a8256071745e1a8334cd92251208bb
Salvador Macias (Softtek) <salvador.macias@ihg.com>
2016-07-06 14:34:14 -0500

    Heroku configurations

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function controllersListen(){
 }
 
 function startServer(){
-  app.listen(3000, function(){
-    console.log('AngularApp started on port 3000');
+  app.listen(process.env.PORT, function(){
+    console.log('AngularApp started on port');
   });
 }
